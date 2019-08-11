6b74f5b61c21db09dd1cd9d99cc54f7a4f2ee151
herrenchek <giselle.herr@icloud.com>
2019-08-11 13:37:16 -0400

    Created trivia object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,12 +5,12 @@ window.onload = function () {
     $("#submit").on("click", reset);
 };
 
-$(document).on("click", "#start", function() {
+$(document).on("click", "#start", function () {
     start();
     // Show Submit button and "quiz" form
     $("#submit").show();
     $("#quiz").show();
-  });
+});
 
 // Global variables
 var interval = 0;
@@ -68,20 +68,63 @@ function timeConverter(t) {
     return minutes + ":" + seconds;
 }
 
-var questions = [
+// Question set
+var trivia = [
     {
-        prompt: "What color are apples?\n(a) Red",
-        answer: "a"
+        question: "Shelob is a character introduced in 'The Two Towers' and is which of the following types of creatures?",
+        options: ["Elf", "Giant spider", "Orc", "Ent"],
+        answer: "Giant spider"
     },
     {
-        prompt: "What color are apples?\n(a) Red",
-        answer: "a"
+        question: "Who is the dwarf featured most prominently in J.R.R. Tolkien's 'Lord of the Rings' series?",
+        options: ["Glóin", "Frodo", "Gimli", "Thorin"],
+        answer: "Gimli"
+    },
+    {
+        question: "Which of the following characters is NOT a memeber of the Fellowship of the Ring?",
+        options: ["Legolas", "Samwise", "Gandalf", "Gollum"],
+        answer: "Gollum"
+    },
+    {
+        question: "Which of the following characters possesses one of the three Elvish Rings of Power?",
+        options: ["Galadriel", "Legolas", "Thranduil", "Celeborn"],
+        answer: "Galadriel"
+    },
+    {
+        question: "What is the name of Gandalf's horse?",
+        options: ["Strider", "Shadowfax", "Fatty Lumpkin", "Snowmane"],
+        answer: "Shadowfax"
+    },
+    {
+        question: "Théoden is betrayed by which of his advisors?",
+        options: ["Éomer", "Éomer", "Grima", "Gandalf"],
+        answer: "Grima"
+    },
+    {
+        question: "Where must Frodo take The One Ring in order for it to be destroyed?",
+        options: ["The Undying Lands", "Erebor", "Barad-dûr", "Mount Doom"],
+        answer: "Mount Doom"
+    },
+    {
+        question: "What type of being is Sauron?",
+        options: ["Maia", "Balrog", "Demon", "Hobbit"],
+        answer: "Maia"
     }
-]
+    {
+        question: "Which of the following fictional creatures is the character Gollum rumored to be inspired by?",
+        options: ["Grendel", "Kappa", "Kraken", "Siren"],
+        answer: "Grendel"
+    }
+    {
+        question: "How many children does the elf Elrond have?",
+        options: ["None", "One", "Two", "Three"],
+        answer: "Three"
+    }
+];
 
 for (var i = 0; i < questions.length; i++) {
     var response = questions.answer;
-    if (response === questions[i].answer) {
+    if (response === trivia[i].answer) {
         score++;
     }
 }
